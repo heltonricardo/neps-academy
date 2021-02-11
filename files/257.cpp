5d3082e5690aefdcc10fdc0602939406696acc8f
Helton Ricardo <50843386+heltonr13@users.noreply.github.com>
2021-02-11 20:22:03 -0300

    Create 257.cpp

---
+++ files/257.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int fib (int n) {
+  if (n < 2) return 1;
+  return fib(n-1) + fib(n-2);
+}
+
+int main(void) {
+  short n;
+
+  cin >> n;
+
+  cout << fib(n) << endl;
+  return 0;
+}
+
